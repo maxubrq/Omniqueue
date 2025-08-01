eb32ced14fda8d6c5f55ca0787d452af416d7a32
Hung Tran (Darius Max) <hungtp.play@gmail.com>
2025-08-01 00:43:11 +0000

    build: update build

--- packages/core/vite.config.js
+++ packages/core/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
         },
         outDir: "dist",
         emptyOutDir: true,
-        target: "es2022"
+        target: "node18"
     },
     plugins: [dts({ insertTypesEntry: true })]
 });
